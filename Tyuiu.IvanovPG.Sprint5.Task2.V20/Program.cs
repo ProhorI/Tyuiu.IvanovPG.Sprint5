5ff3808f8518781fad821812b4e4c1b69a413bd7
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-11-23 01:12:54 +0500

    Task2

---
+++ Tyuiu.IvanovPG.Sprint5.Task2.V20/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.IvanovPG.Sprint5.Task2.V20.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint5.Task2.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { { 5, -5, -1 },
+                                          {-4, 2, -4 },
+                                          { -7, 1, 4 }};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;  
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Иванов.П.Г. | ИСТН-б-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнила: Иванов.П.Г.                    | ИСТН-б-24-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
