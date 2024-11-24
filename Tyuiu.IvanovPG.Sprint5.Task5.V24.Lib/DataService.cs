a48ff6b81cd635ef057fd809322cc21fb1d70fb4
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-11-24 18:46:27 +0500

    Task5

--- Tyuiu.IvanovPG.Sprint5.Task5.V24.Lib/DataService.cs
+++ Tyuiu.IvanovPG.Sprint5.Task5.V24.Lib/DataService.cs
@@ -20,7 +20,7 @@ namespace Tyuiu.IvanovPG.Sprint5.Task5.V24.Lib
                     {
                         double lineParse = double.Parse(number);
 
-                        if (lineParse % 1 == 0)
+                        if (lineParse % 1 == 0 && lineParse >= 10)
                         {
                             res += lineParse;
                         }
