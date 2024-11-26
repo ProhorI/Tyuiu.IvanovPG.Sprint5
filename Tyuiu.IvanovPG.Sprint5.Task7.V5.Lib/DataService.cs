4473c11a912657c1febd4dc974409b952413e903
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-11-27 00:37:30 +0500

    Task7

--- Tyuiu.IvanovPG.Sprint5.Task7.V5.Lib/DataService.cs
+++ Tyuiu.IvanovPG.Sprint5.Task7.V5.Lib/DataService.cs
@@ -1,6 +1,7 @@
 ﻿
 using tyuiu.cources.programming.interfaces.Sprint5;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace Tyuiu.IvanovPG.Sprint5.Task7.V5.Lib
 {
@@ -9,34 +10,28 @@ namespace Tyuiu.IvanovPG.Sprint5.Task7.V5.Lib
         public string LoadDataAndSave(string path)
         {
             string pathSaveFile = Path.Combine(Path.GetTempPath(), "InPutDataFileTask7V5.txt");
+            string outText = "";
+            string alp = "[a-z]";
 
-            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
 
             if (fileExists)
             {
                 File.Delete(pathSaveFile);
             }
-
-            string strLine = "";
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    for (int i = 0; i < line.Length; i++)
-                    {
-                        if (((line[i] >= 'а') && (line[i] <= 'я')) || ((line[i] >= 'А') && (line[i] <= 'Я')) || line[i] == ' ' || line[i] == '!' || line[i] == ',' || line[i] == '.')
-                        {
-                            strLine = strLine + line[i];
-                        }
-                    }
-
-                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
-                    strLine = "";
+                    outText += Regex.Replace(line, alp, "", RegexOptions.IgnoreCase);
+                    outText = outText.Replace("    ", "");
                 }
+                File.AppendAllText(pathSaveFile, outText);
+                return pathSaveFile;
             }
-            return pathSaveFile;
         }
     }
 }
+//if (((line[i] >= 'а') && (line[i] <= 'я')) || ((line[i] >= 'А') && (line[i] <= 'Я')) || line[i] == ' ' || line[i] == '!' || line[i] == ',' || line[i] == '.')
